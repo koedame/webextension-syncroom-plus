b04f0c4dae60847d9a79cd001980734f1046db3f
ahlisohxu8faigai0ohv3phuwiomiil5 <69432552+ahlisohxu8faigai0ohv3phuwiomiil5@users.noreply.github.com>
2020-08-12 06:24:10 +0900

    storeを整理

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import VuexWebExtensions from 'vuex-webextensions';
-
 import rooms from './rooms.js';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  plugins: [
-    VuexWebExtensions({
-      // persistentStates: ['currentTabUrl'],
-    }),
-  ],
-  // state: {
-  //   currentTabUrl: '',
-  // },
   modules: {
     rooms: rooms,
   },
