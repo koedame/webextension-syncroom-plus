ab7ef11b79456e238b21bb41446645f0d6d8a9a2
ahlisohxu8faigai0ohv3phuwiomiil5 <69432552+ahlisohxu8faigai0ohv3phuwiomiil5@users.noreply.github.com>
2020-08-14 17:14:40 +0900

    お気に入り機能に追加

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import clock from './clock.js';
+import favoriteMembers from './favorite_members.js';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     clock: clock,
+    favoriteMembers: favoriteMembers,
   },
 });
