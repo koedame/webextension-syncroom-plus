f69f4f80048908d2b61dcf4b72f5cfdc26f02da0
ahlisohxu8faigai0ohv3phuwiomiil5 <69432552+ahlisohxu8faigai0ohv3phuwiomiil5@users.noreply.github.com>
2020-08-15 04:13:34 +0900

    firefox用にビルドスクリプト追加

--- scripts/build-zip.js
+++ scripts/build-zip.js
@@ -5,29 +5,29 @@ const path = require('path');
 const archiver = require('archiver');
 
 const DEST_DIR = path.join(__dirname, '../dist');
-const DEST_ZIP_DIR = path.join(__dirname, '../dist-zip'); 
+const DEST_ZIP_DIR = path.join(__dirname, '../dist-zip');
 
 const extractExtensionData = () => {
   const extPackageJson = require('../package.json');
 
   return {
     name: extPackageJson.name,
-    version: extPackageJson.version
-  }
+    version: extPackageJson.version,
+  };
 };
 
 const makeDestZipDirIfNotExists = () => {
-  if(!fs.existsSync(DEST_ZIP_DIR)) {
+  if (!fs.existsSync(DEST_ZIP_DIR)) {
     fs.mkdirSync(DEST_ZIP_DIR);
   }
-}
+};
 
 const buildZip = (src, dist, zipFilename) => {
   console.info(`Building ${zipFilename}...`);
 
-  const archive = archiver('zip', { zlib: { level: 9 }});
+  const archive = archiver('zip', { zlib: { level: 9 } });
   const stream = fs.createWriteStream(path.join(dist, zipFilename));
-  
+
   return new Promise((resolve, reject) => {
     archive
       .directory(src, false)
@@ -40,14 +40,19 @@ const buildZip = (src, dist, zipFilename) => {
 };
 
 const main = () => {
-  const {name, version} = extractExtensionData();
-  const zipFilename = `${name}-v${version}.zip`;
-  
+  const { name, version } = extractExtensionData();
+
   makeDestZipDirIfNotExists();
 
-  buildZip(DEST_DIR, DEST_ZIP_DIR, zipFilename)
+  // Google Chrome
+  buildZip(DEST_DIR, DEST_ZIP_DIR, `GoogleChrome-${name}-v${version}.zip`)
     .then(() => console.info('OK'))
-    .catch(console.err); 
+    .catch(console.err);
+
+  // Mozilla Firefox
+  buildZip(DEST_DIR, DEST_ZIP_DIR, `MozillaFirefox-${name}-v${version}.xpi`)
+    .then(() => console.info('OK'))
+    .catch(console.err);
 };
 
 main();
