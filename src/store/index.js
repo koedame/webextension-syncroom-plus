d943d2ca8995bca34f2954578f458c110fc2ae65
ahlisohxu8faigai0ohv3phuwiomiil5 <69432552+ahlisohxu8faigai0ohv3phuwiomiil5@users.noreply.github.com>
2020-08-14 18:50:05 +0900

    複数Tabでもstateを共有するように設定

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ import clock from './clock.js';
 import favoriteMembers from './favorite_members.js';
 
 import VuexPersistence from 'vuex-persist';
+import createMutationsSharer from 'vuex-shared-mutations';
 
 global.browser = require('webextension-polyfill');
 
@@ -14,10 +15,26 @@ const vuexLocal = new VuexPersistence({
 
 Vue.use(Vuex);
 
+console.log(favoriteMembers.mutations);
+
 export default new Vuex.Store({
   modules: {
     clock: clock,
     favoriteMembers: favoriteMembers,
   },
-  plugins: [vuexLocal.plugin],
+  plugins: [
+    // stateをlocalStrageに保存して永続化
+    vuexLocal.plugin,
+    // 複数Tab/Windowでのstateの共有
+    createMutationsSharer({
+      predicate: (mutation, state) => {
+        const predicate = Object.keys(favoriteMembers.mutations).map(name => `favoriteMembers/${name}`);
+        // Conditionally trigger other plugins subscription event here to
+        // have them called only once (in the tab where the commit happened)
+        // ie. save certain values to localStorage
+        // pluginStateChanged(mutation, state)
+        return predicate.indexOf(mutation.type) >= 0;
+      },
+    }),
+  ],
 });
