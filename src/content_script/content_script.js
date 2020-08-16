06556f1182cde36cb763c087631a001e5a825e0f
ahlisohxu8faigai0ohv3phuwiomiil5 <69432552+ahlisohxu8faigai0ohv3phuwiomiil5@users.noreply.github.com>
2020-08-16 19:43:03 +0900

    lintin

--- src/content_script/content_script.js
+++ src/content_script/content_script.js
@@ -2,19 +2,18 @@ import Vue from 'vue';
 import App from './App';
 import store from '../store';
 
-global.browser = require('webextension-polyfill');
-Vue.prototype.$browser = global.browser;
-
 // FontAwesomeIcon
 import { library } from '@fortawesome/fontawesome-svg-core';
-
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
-
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 library.add(fas, far, fab);
 Vue.component('fa', FontAwesomeIcon);
+
+global.browser = require('webextension-polyfill');
+Vue.prototype.$browser = global.browser;
 
 // stateを復元
 store.dispatch('favoriteMembers/restoreFromLocalStorage');
