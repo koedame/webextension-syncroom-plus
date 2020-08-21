16dea64e39d6b63735487ed23355207a422331dc
💩 <69432552+ahlisohxu8faigai0ohv3phuwiomiil5@users.noreply.github.com>
2020-08-22 05:51:59 +0900

    background.js => background.ts

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,6 @@
-//@ts-ignore
+import makeJoinUri from './lib/make_join_uri';
 import store from './store';
+import { NotificationVacancyRoom } from './store/types';
 import axios from 'axios';
 
 const browser = require('webextension-polyfill');
@@ -41,18 +42,12 @@ setInterval((): void => {
   store.dispatch('notificationVacancyRooms/restoreFromLocalStorage');
   store.dispatch('notificationOnlineMembers/restoreFromLocalStorage');
 
-  // 通知登録がある場合のみ実行
-  const notificationVacancyRooms = store.getters['notificationVacancyRooms/rooms'];
-  const notificationOnlineMembers = store.getters['notificationOnlineMembers/members'];
-
-  if (notificationVacancyRooms.length !== 0 || notificationOnlineMembers.length !== 0) {
+  if (store.getters['notificationVacancyRooms/rooms'].length !== 0 || store.getters['notificationOnlineMembers/members'].length !== 0) {
     axios.get('https://webapi.syncroom.appservice.yamaha.com/ndroom/room_list.json?pagesize=500&realm=4').then((res) => {
-      const rooms: any = res.data.rooms;
-
-      for (let room of rooms) {
+      for (let room of res.data.rooms) {
         // オンライン通知
         for (let member of room.members) {
-          const notificationOnlineMember = notificationOnlineMembers.find((r: any) => r.memberName === member);
+          const notificationOnlineMember = store.getters['notificationOnlineMembers/members'].find((r: any) => r.memberName === member);
           // 最後の部屋作成日時と違う場合は通知
           if (notificationOnlineMember && notificationOnlineMember.lastNotificationRoomCreatedTime !== room.create_time) {
             const options: any = {
@@ -78,7 +73,7 @@ setInterval((): void => {
         // 空き通知
         if (room.num_members < 5) {
           const uid: string = `${room.create_time}||${room.room_name}`;
-          const isExistNotificationVacancyRooms: boolean = notificationVacancyRooms.some((r: any) => r.uid === uid);
+          const isExistNotificationVacancyRooms: boolean = store.getters['notificationVacancyRooms/rooms'].some((r: any) => r.uid === uid);
           if (isExistNotificationVacancyRooms) {
             const options: any = {
               type: 'basic',
@@ -99,8 +94,8 @@ setInterval((): void => {
       }
 
       // roomがなくなっていれば通知を削除
-      for (let notificationVacancyRoom of notificationVacancyRooms) {
-        if (!rooms.some((r: any) => `${r.create_time}||${r.room_name}` === notificationVacancyRoom.uid)) {
+      for (let notificationVacancyRoom of store.getters['notificationVacancyRooms/rooms']) {
+        if (!res.data.rooms.some((r: any) => `${r.create_time}||${r.room_name}` === notificationVacancyRoom.uid)) {
           store.dispatch('notificationVacancyRooms/removeNotificationByUID', notificationVacancyRoom.uid);
           // 通知が残っていれば消しておく
           browser.notifications.clear(notificationVacancyRoom.uid);
@@ -109,45 +104,6 @@ setInterval((): void => {
     });
   }
 }, 1000);
-
-const makeJoinUri = (roomName: string, pass: any, pid: number, mode: number): string => {
-  let urienc = (str: string | number): string => {
-    return encodeURIComponent(str).replace(/[!*'()]/g, (c) => {
-      return '%' + c.charCodeAt(0).toString(16);
-    });
-  };
-
-  let str: string = 'joingroup?mode=' + urienc(mode) + '&pid=' + urienc(pid) + '&nickname=&groupname=' + urienc(roomName) + '&password=' + urienc(pass);
-  let uri: string = 'syncroom:';
-  let tbl: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-  let len: number = str.length;
-  let mod: number = len % 3;
-  if (mod > 0) len -= mod;
-
-  let i, t;
-  for (i = 0; i < len; i += 3) {
-    t = (str.charCodeAt(i + 0) << 16) | (str.charCodeAt(i + 1) << 8) | str.charCodeAt(i + 2);
-    uri += tbl.charAt((t >> 18) & 0x3f);
-    uri += tbl.charAt((t >> 12) & 0x3f);
-    uri += tbl.charAt((t >> 6) & 0x3f);
-    uri += tbl.charAt(t & 0x3f);
-  }
-  if (mod === 2) {
-    t = (str.charCodeAt(i + 0) << 16) | (str.charCodeAt(i + 1) << 8);
-    uri += tbl.charAt((t >> 18) & 0x3f);
-    uri += tbl.charAt((t >> 12) & 0x3f);
-    uri += tbl.charAt((t >> 6) & 0x3f);
-    uri += '=';
-  } else if (mod === 1) {
-    t = str.charCodeAt(i + 0) << 16;
-    uri += tbl.charAt((t >> 18) & 0x3f);
-    uri += tbl.charAt((t >> 12) & 0x3f);
-    uri += '=';
-    uri += '=';
-  }
-
-  return uri;
-};
 
 browser.notifications.onClicked.addListener((notificationId: string): void => {
   const splittedNotificationId: Array<string> = notificationId.split('::');
