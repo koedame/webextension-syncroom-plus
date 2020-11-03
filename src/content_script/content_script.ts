996f5f2c02c0dfd0c919bd492ae700586b8c2a77
💩 <69432552+ahlisohxu8faigai0ohv3phuwiomiil5@users.noreply.github.com>
2020-11-03 19:18:32 +0900

    fontawesome削除

--- src/content_script/content_script.ts
+++ src/content_script/content_script.ts
@@ -36,9 +36,7 @@ import Buefy from 'buefy';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-library.add(fas, far, fab);
+library.add(fas);
 Vue.component('vue-fontawesome', FontAwesomeIcon);
 Vue.use(Buefy, {
   defaultIconComponent: 'vue-fontawesome',
