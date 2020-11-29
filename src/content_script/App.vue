29cd83d59917be783cb23a9b4564222eef9b116f
💩 <69432552+unchidev@users.noreply.github.com>
2020-11-30 05:21:20 +0900

    ツールチップが途切れる不具合を修正

--- src/content_script/App.vue
+++ src/content_script/App.vue
@@ -323,31 +323,6 @@ export default {
 // Import Bulma's core
 @import "~bulma/sass/utilities/_all"
 
-// // Set your colors
-// $primary: #8c67ef;
-// $primary-invert: findColorInvert($primary);
-// $twitter: #4099FF;
-// $twitter-invert: findColorInvert($twitter);
-
-// // Setup $colors to use as bulma classes (e.g. 'is-twitter')
-// $colors: (
-//     "white": ($white, $black),
-//     "black": ($black, $white),
-//     "light": ($light, $light-invert),
-//     "dark": ($dark, $dark-invert),
-//     "primary": ($primary, $primary-invert),
-//     "info": ($info, $info-invert),
-//     "success": ($success, $success-invert),
-//     "warning": ($warning, $warning-invert),
-//     "danger": ($danger, $danger-invert),
-//     "twitter": ($twitter, $twitter-invert)
-// );
-
-// // Links
-// $link: $primary;
-// $link-invert: $primary-invert;
-// $link-focus-border: $primary;
-
 // Import Bulma and Buefy styles
 @import "~bulma"
 @import "~buefy/src/scss/buefy"
@@ -393,6 +368,8 @@ export default {
 
 .room-list-item
   transition: all 500ms
+  // `.card` に `overflow: hidden` が設定されているので上書き
+  overflow: visible
 
 .room-list-enter
   opacity: 0
@@ -414,4 +391,6 @@ export default {
   position: fixed
   bottom: 20px
   right: 12px
+
+.card
 </style>
