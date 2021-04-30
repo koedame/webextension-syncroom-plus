bc4aa327c177532de5e94233eb1799a17003fe52
💩 <69432552+unchidev@users.noreply.github.com>
2021-05-01 08:46:43 +0900

    lintin

--- src/store/types.ts
+++ src/store/types.ts
@@ -2,17 +2,13 @@ export interface RootState {
   version: string;
 }
 
-export interface FavoriteMembersState {
-  members: FavoriteMember[];
-}
-
 export interface FavoriteMember {
   memberName: string;
   createdAt: string;
 }
 
-export interface NotificationOnlineMembersState {
-  members: NotificationOnlineMember[];
+export interface FavoriteMembersState {
+  members: FavoriteMember[];
 }
 
 export interface NotificationOnlineMember {
@@ -21,13 +17,17 @@ export interface NotificationOnlineMember {
   lastNotificationRoomCreatedTime: string;
 }
 
-export interface NotificationVacancyRoomsState {
-  rooms: NotificationVacancyRoom[];
+export interface NotificationOnlineMembersState {
+  members: NotificationOnlineMember[];
 }
 
 export interface NotificationVacancyRoom {
   uid: string;
   createdAt: string;
+}
+
+export interface NotificationVacancyRoomsState {
+  rooms: NotificationVacancyRoom[];
 }
 
 export interface ConfigState {
