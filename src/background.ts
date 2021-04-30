1dfaa8c963505e0a759f4bfcee50d58462026f8a
💩 <69432552+unchidev@users.noreply.github.com>
2021-04-30 18:03:47 +0900

    background.tsをリファクタリング

--- src/background.ts
+++ src/background.ts
@@ -44,7 +44,7 @@ browser.notifications.onClosed.addListener((notificationId: string): void => {
   browser.notifications.clear(notificationId);
 });
 
-setInterval(async () => {
+setInterval(() => {
   browser.storage.local.get('configLanguage').then(({ configLanguage }) => {
     if (typeof configLanguage === 'undefined') {
       i18n.locale = 'ja';
@@ -53,30 +53,76 @@ setInterval(async () => {
     }
   });
 
-  const res = await axios.get('http://localhost:8080/api/v1/rooms/all');
+  // FIXME: URLを変更
+  axios.get('http://localhost:8080/api/v1/rooms/all').then((res) => {
+    // FIXME: オフラインになったら通知を削除
+    // ユーザーオンライン通知
+    browser.storage.local.get('notificationOnlineMembers').then(({ notificationOnlineMembers }) => {
+      // データがなければ何もしない
+      if (!Array.isArray(notificationOnlineMembers)) {
+        return false;
+      }
+      if (notificationOnlineMembers.length === 0) {
+        return false;
+      }
 
-  // FIXME: オフラインになったら通知を削除
-  // ユーザーオンライン通知
-  browser.storage.local.get('notificationOnlineMembers').then(({ notificationOnlineMembers }) => {
-    // データがなければ何もしない
-    if (!Array.isArray(notificationOnlineMembers)) {
-      return false;
-    }
-    if (notificationOnlineMembers.length === 0) {
-      return false;
-    }
+      for (const room of res.data.rooms) {
+        for (const member of room.members) {
+          const notificationOnlineMember = notificationOnlineMembers.find((m: any) => m.memberName === member.name);
 
-    for (const room of res.data.rooms) {
-      for (const member of room.members) {
-        const notificationOnlineMember = notificationOnlineMembers.find((m: any) => m.memberName === member.name);
+          // 登録されているメンバーでないときは何もしない
+          if (typeof notificationOnlineMember === 'undefined') {
+            continue;
+          }
 
-        // 登録されているメンバーでないときは何もしない
-        if (typeof notificationOnlineMember === 'undefined') {
+          // すでに通知済みであれば何もしない
+          if (notificationOnlineMember.lastNotificationRoomCreatedTime === room.created_at) {
+            continue;
+          }
+
+          const options: any = {
+            type: 'basic',
+            // TODO: iconをわかりやすいものに変える
+            iconUrl: 'images/icon_128.png',
+            title: `${translate('room_name')}：${room.name}`,
+            message: translate('online_user', { username: notificationOnlineMember.memberName }),
+          };
+
+          if (currentBrowser === 'GoogleChrome') {
+            // このオプションはGoogleChromeしか対応していないので判定して追加する
+            // 参考: https://developer.mozilla.org/ja/docs/Mozilla/Add-ons/WebExtensions/API/notifications/NotificationOptions
+            options.requireInteraction = true;
+          }
+          browser.notifications.create(`online_member::${notificationOnlineMember.memberName}`, options);
+
+          // 最終通知を更新
+          notificationOnlineMember.lastNotificationRoomCreatedTime = room.created_at;
+          browser.storage.local.set({
+            notificationOnlineMembers: JSON.parse(JSON.stringify(notificationOnlineMembers)),
+          });
+        }
+      }
+    });
+
+    // TODO: 部屋がなくなれば通知を削除
+    browser.storage.local.get('notificationVacancyRooms').then(({ notificationVacancyRooms }) => {
+      // データがなければ何もしない
+      if (!Array.isArray(notificationVacancyRooms)) {
+        return false;
+      }
+      if (notificationVacancyRooms.length === 0) {
+        return false;
+      }
+
+      for (const room of res.data.rooms) {
+        // 満室の部屋はスキップ
+        if (room.members.length === 5) {
           continue;
         }
 
-        // すでに通知済みであれば何もしない
-        if (notificationOnlineMember.lastNotificationRoomCreatedTime === room.created_at) {
+        const uid: string = `${room.created_at}||${room.name}`;
+        // 通知登録に存在しなければスキップ
+        if (!notificationVacancyRooms.some((r: any) => r.uid === uid)) {
           continue;
         }
 
@@ -85,66 +131,21 @@ setInterval(async () => {
           // TODO: iconをわかりやすいものに変える
           iconUrl: 'images/icon_128.png',
           title: `${translate('room_name')}：${room.name}`,
-          message: translate('online_user', { username: notificationOnlineMember.memberName }),
+          message: translate('you_can_now_join'),
         };
-
         if (currentBrowser === 'GoogleChrome') {
           // このオプションはGoogleChromeしか対応していないので判定して追加する
           // 参考: https://developer.mozilla.org/ja/docs/Mozilla/Add-ons/WebExtensions/API/notifications/NotificationOptions
           options.requireInteraction = true;
         }
-        browser.notifications.create(`online_member::${notificationOnlineMember.memberName}`, options);
+        browser.notifications.create(`vacancy::${uid}`, options);
 
-        // 最終通知を更新
-        notificationOnlineMember.lastNotificationRoomCreatedTime = room.created_at;
+        // 通知したらキューを削除しておく
+        const newNotificationVacancyRooms = notificationVacancyRooms.filter((r) => r.uid !== uid);
         browser.storage.local.set({
-          notificationOnlineMembers: JSON.parse(JSON.stringify(notificationOnlineMembers)),
+          notificationVacancyRooms: JSON.parse(JSON.stringify(newNotificationVacancyRooms)),
         });
       }
-    }
+    });
   });
-
-  // TODO: 部屋がなくなれば通知を削除
-  browser.storage.local.get('notificationVacancyRooms').then(({ notificationVacancyRooms }) => {
-    // データがなければ何もしない
-    if (!Array.isArray(notificationVacancyRooms)) {
-      return false;
-    }
-    if (notificationVacancyRooms.length === 0) {
-      return false;
-    }
-
-    for (const room of res.data.rooms) {
-      // 満室の部屋はスキップ
-      if (room.members.length === 5) {
-        continue;
-      }
-
-      const uid: string = `${room.created_at}||${room.name}`;
-      // 通知登録に存在しなければスキップ
-      if (!notificationVacancyRooms.some((r: any) => r.uid === uid)) {
-        continue;
-      }
-
-      const options: any = {
-        type: 'basic',
-        // TODO: iconをわかりやすいものに変える
-        iconUrl: 'images/icon_128.png',
-        title: `${translate('room_name')}：${room.name}`,
-        message: translate('you_can_now_join'),
-      };
-      if (currentBrowser === 'GoogleChrome') {
-        // このオプションはGoogleChromeしか対応していないので判定して追加する
-        // 参考: https://developer.mozilla.org/ja/docs/Mozilla/Add-ons/WebExtensions/API/notifications/NotificationOptions
-        options.requireInteraction = true;
-      }
-      browser.notifications.create(`vacancy::${uid}`, options);
-
-      // 通知したらキューを削除しておく
-      const newNotificationVacancyRooms = notificationVacancyRooms.filter((r) => r.uid !== uid);
-      browser.storage.local.set({
-        notificationVacancyRooms: JSON.parse(JSON.stringify(newNotificationVacancyRooms)),
-      });
-    }
-  });
-}, 1000);
+}, 3000);
