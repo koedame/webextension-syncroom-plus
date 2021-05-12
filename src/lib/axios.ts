5b23304f55d2896c17ed10392cfc27ec8cc38eda
💩 <69432552+unchidev@users.noreply.github.com>
2021-05-12 22:45:07 +0900

    lintin

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import { browser } from 'webextension-polyfill-ts';
 
 const axiosClient = axios.create({
   baseURL: 'https://syncroomplus.koeda.me/',
-  headers: { 'X-SYNCROOM-Plus-Version': browser.runtime.getManifest().version }
+  headers: { 'X-SYNCROOM-Plus-Version': browser.runtime.getManifest().version },
 });
 
 export default axiosClient;
