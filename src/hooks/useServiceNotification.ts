9a27a770f34320d0be135ba04c7947079a7e7277
💩 <69432552+unchidev@users.noreply.github.com>
2022-07-10 13:22:29 +0900

    useServiceNotificationを作成

---
+++ src/hooks/useServiceNotification.ts
@@ -0,0 +1,42 @@
+import { DateTime } from "luxon";
+import { useEffect, useState } from "react";
+import { ServiceNotificationRepository } from "../repositories/serviceNotificationRepository";
+import { SYNCROOMPlus } from "../types/syncroomPlus";
+
+export const useServiceNotification = () => {
+  const [color, setColor] = useState<'blue' | 'green' | 'yellow' | 'red'>('yellow');
+  const [isShow, setIsShow] = useState<boolean>(false);
+  const [serviceNotification, setServiceNotification] = useState<SYNCROOMPlus.ServiceNotificationType | null>(null);
+
+  const fetchServiceNotification = () => {
+    ServiceNotificationRepository.latest().then((res) => {
+      setServiceNotification(res);
+    }).catch((error) => {
+
+      // エラーで停止しないようにキャッチして握りつぶしておく
+      console.error('お知らせ取得失敗', error);
+    })
+  };
+
+  useEffect(() => {
+    if (serviceNotification) {
+      // 通知期間の判定
+      const now = DateTime.now();
+      setIsShow(DateTime.fromISO(serviceNotification.start_at) < now && DateTime.fromISO(serviceNotification.end_at) > now);
+
+      // メインカラーを設定
+      if (serviceNotification.notification_type === 'info') setColor('blue');
+      else if (serviceNotification.notification_type === 'success') setColor('green');
+      else if (serviceNotification.notification_type === 'warning') setColor('yellow');
+      else if (serviceNotification.notification_type === 'danger') setColor('red');
+      else setColor('blue');
+    }
+  }, [serviceNotification]);
+
+  return {
+    color,
+    isShow,
+    serviceNotification,
+    fetchServiceNotification
+  };
+};
