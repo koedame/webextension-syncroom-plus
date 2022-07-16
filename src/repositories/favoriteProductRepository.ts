a684c72616fb4924acc53871ee58b6eccceee50a
💩 <69432552+unchidev@users.noreply.github.com>
2022-07-17 03:20:06 +0900

    プロフィール編集機能追加

---
+++ src/repositories/favoriteProductRepository.ts
@@ -0,0 +1,25 @@
+import { SYNCROOM } from '../types/syncroom';
+
+// 興味のある製品管理
+export const FavoriteProductRepository = {
+  // 興味のある製品一覧
+  index: (): SYNCROOM.FavoriteProductType[] => {
+    return [
+      'SYNCROOM Plus',
+      'ピアノ・電子ピアノ',
+      'エレクトーン・キーボード',
+      'シンセサイザー',
+      '音楽制作',
+      'DJ',
+      '管楽器',
+      '弦楽器',
+      'ギター',
+      'ベース',
+      'ドラム',
+      'パーカッション',
+      'PA機器',
+      'ホームシアター・オーディオ',
+      'その他',
+    ];
+  },
+};
