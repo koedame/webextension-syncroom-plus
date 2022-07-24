a75a7fc3884f44f069991804fa8db3a6fce2015e
💩 <69432552+unchidev@users.noreply.github.com>
2022-07-25 00:05:53 +0900

    プロフィール画像設定とツイッター連携設定が行えるように変更

--- src/repositories/sessionRepository.ts
+++ src/repositories/sessionRepository.ts
@@ -18,6 +18,48 @@ export const SessionRepository = {
     return res.json();
   },
 
+  // ツイッター連携設定更新
+  async updateTwitterConfig(params: {
+    profileLinked: {
+      type: 'twitter';
+      linkNickname: boolean;
+      linkImage: boolean;
+    };
+    autoTweet: {
+      roomCreated: boolean;
+    };
+  }): Promise<{ status: 'ok' }> {
+    const res = await srClientWithToken('https://webapi.syncroom.appservice.yamaha.com/comm/users/me/profile', {
+      method: 'post',
+      body: JSON.stringify(params),
+    });
+    return res.json();
+  },
+
+  // ツイッター連携用URL取得
+  async getConnectTwitterLink(): Promise<{ url: string }> {
+    const res = await srClientWithToken('https://webapi.syncroom.appservice.yamaha.com/comm/twitter/link_url?callbackURL=https%3A%2F%2Fsyncroom.yamaha.com%2Fplay%2F', {
+      method: 'get',
+    });
+    return res.json();
+  },
+
+  // ツイッター連携解除
+  async unlinkTwitter(): Promise<SYNCROOM.MyProfileType> {
+    const res = await srClientWithToken('https://webapi.syncroom.appservice.yamaha.com/comm/users/me/profile', {
+      method: 'post',
+      body: JSON.stringify({
+        profileLinked: {
+          type: 'none',
+          linkNickname: false,
+          linkImage: false,
+        },
+        autoTweet: false,
+      }),
+    });
+    return res.json();
+  },
+
   // ログアウト処理
   async logout(): Promise<{ status: 'ok' }> {
     const res = await srClientWithToken('https://webapi.syncroom.appservice.yamaha.com/comm/logout', {
