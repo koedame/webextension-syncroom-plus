638c88b02b1d234fc7a570431d95bf2ed1dcfa2d
💩 <69432552+unchidev@users.noreply.github.com>
2022-09-17 21:14:53 +0900

    prettierin

--- src/__tests__/lib/decodeJWTPayLoad.test.ts
+++ src/__tests__/lib/decodeJWTPayLoad.test.ts
@@ -8,19 +8,20 @@ import decodeJWTPayLoad from '../../lib/decodeJWTPayLoad';
 describe('正常なペイロードが含まれるJWTのとき', () => {
   it('ペイロードが返ること', () => {
     // echo -n  '{"aud":"syncroom","exp":1641081600,"iat":1641081600,"iss":"https://webapi.syncroom.appservice.yamaha.com","key_type":2,"sub":"11111111-1111-1111-1111-111111111111","user_id":"11111111-1111-1111-1111-111111111111","x-logid":"1111111111111111111111111111111111111111111111111111111111111111"}' | base64
-    const validToken = '__header__.eyJhdWQiOiJzeW5jcm9vbSIsImV4cCI6MTY0MTA4MTYwMCwiaWF0IjoxNjQxMDgxNjAwLCJpc3MiOiJodHRwczovL3dlYmFwaS5zeW5jcm9vbS5hcHBzZXJ2aWNlLnlhbWFoYS5jb20iLCJrZXlfdHlwZSI6Miwic3ViIjoiMTExMTExMTEtMTExMS0xMTExLTExMTEtMTExMTExMTExMTExIiwidXNlcl9pZCI6IjExMTExMTExLTExMTEtMTExMS0xMTExLTExMTExMTExMTExMSIsIngtbG9naWQiOiIxMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExIn0=.__sign__';
+    const validToken =
+      '__header__.eyJhdWQiOiJzeW5jcm9vbSIsImV4cCI6MTY0MTA4MTYwMCwiaWF0IjoxNjQxMDgxNjAwLCJpc3MiOiJodHRwczovL3dlYmFwaS5zeW5jcm9vbS5hcHBzZXJ2aWNlLnlhbWFoYS5jb20iLCJrZXlfdHlwZSI6Miwic3ViIjoiMTExMTExMTEtMTExMS0xMTExLTExMTEtMTExMTExMTExMTExIiwidXNlcl9pZCI6IjExMTExMTExLTExMTEtMTExMS0xMTExLTExMTExMTExMTExMSIsIngtbG9naWQiOiIxMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExMTExIn0=.__sign__';
     expect(decodeJWTPayLoad(validToken)).toStrictEqual({
-      "aud": "syncroom",
-      "exp": Number(new Date('2022-01-02 00:00:00Z')) / 1000,
-      "iat": Number(new Date('2022-01-02 00:00:00Z')) / 1000,
-      "iss": "https://webapi.syncroom.appservice.yamaha.com",
-      "key_type": 2,
-      "sub": "11111111-1111-1111-1111-111111111111",
-      "user_id": "11111111-1111-1111-1111-111111111111",
-      "x-logid": "1111111111111111111111111111111111111111111111111111111111111111",
+      aud: 'syncroom',
+      exp: Number(new Date('2022-01-02 00:00:00Z')) / 1000,
+      iat: Number(new Date('2022-01-02 00:00:00Z')) / 1000,
+      iss: 'https://webapi.syncroom.appservice.yamaha.com',
+      key_type: 2,
+      sub: '11111111-1111-1111-1111-111111111111',
+      user_id: '11111111-1111-1111-1111-111111111111',
+      'x-logid': '1111111111111111111111111111111111111111111111111111111111111111',
     });
   });
-})
+});
 
 describe('JWT形式の文字列ではないとき', () => {
   it('エラーになること', () => {
